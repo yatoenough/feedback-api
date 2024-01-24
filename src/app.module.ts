deb40880b12444803169d3bb56c12b403368ca5d
yatoenough <yatoenough@gmail.com>
2024-01-24 02:07:35 +0100

    Initialized settings module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmOptions } from './database/database.config';
+import { SettingsModule } from './modules/settings/settings.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     TypeOrmModule.forRoot(TypeOrmOptions),
+    SettingsModule,
   ],
   controllers: [],
   providers: [],
